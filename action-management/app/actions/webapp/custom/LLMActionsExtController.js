df9b94829a67667a84a2fbf272e4a61c89ede03d
Praveen Kumar Padegal <p.padegal@sap.com>
2023-10-18 12:13:10 +0000

    updated llm ui

--- action-management/app/actions/webapp/custom/LLMActionsExtController.js
+++ action-management/app/actions/webapp/custom/LLMActionsExtController.js
@@ -1,8 +1,9 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-undef */
-sap.ui.define(["sap/m/MessageBox", "sap/m/MessageToast",  "sap/ui/model/json/JSONModel"], 
-function (MessageBox, MessageToast, JSONModel){
+sap.ui.define(["sap/m/MessageBox", "sap/m/MessageToast",  "sap/ui/model/json/JSONModel", "sap/f/library"], 
+function (MessageBox, MessageToast, JSONModel, fioriLibrary){
     let bFragmentLoaded=false;
+    let llmResponse = "";
     "use strict";
     function _createUploadController(oExtensionAPI) {
         var oUploadDialog;
@@ -47,16 +48,40 @@ function (MessageBox, MessageToast, JSONModel){
                 oUploadDialog && oUploadDialog.close();
             },
 
+            onListItemPress: function(oEvent){
+                this.fcl.setLayout(fioriLibrary.LayoutType.TwoColumnsMidExpanded);
+                let idSplitList = oEvent.getParameters()["id"].split('-');
+                let index = parseInt(idSplitList[idSplitList.length-1]);
+                // debugger;
+                let LLMItem = oEvent.getSource().getModel("LLM").getData().Actions[index];
+                LLMItem = JSON.parse(JSON.stringify(LLMItem));
+                LLMItem.Action_Payload = JSON.stringify(LLMItem.Action_Payload)
+                let oLLMItemModel = new JSONModel({Action: LLMItem});
+                this.detailPage.setModel(oLLMItemModel, "LLMItem");
+                // var oFCL = this.oView.getParent().getParent();
+                // oFCL.setLayout(fioriLibrary.LayoutType.TwoColumnsMidExpanded);
+            },
+
             onGetActionsFromLLM: function(oEvent) {
+                // let oVBox = oEvent.getSource().getParent();
+                // let oTextAreaInput = oVBox.getAggregation("items")[0];
                 let oVBox = oEvent.getSource().getParent();
-                let oTextAreaInput = oVBox.getAggregation("items")[0];
+                let oTextAreaInput = oVBox.getAggregation("content")[0];
                 const userInputForLLM = oTextAreaInput.getValue();
                 console.log(userInputForLLM);
 
                 //call llm and get suggested actions
                 let oDialog1 = oVBox.getParent();
-                let oHBox = oDialog1.getAggregation("content")[1];
+                // let oHBox = oDialog1.getAggregation("content")[1];
+                let oHBox = oVBox.getAggregation("content")[3];
                 let oOperation = oHBox.getObjectBinding();
+
+                // this.fcl = oDialog1.getAggregation("content")[0].getAggregation("items")[2]
+                this.fcl = oVBox.getAggregation("content")[2].getAggregation("items")[0]
+                this.beginPage = this.fcl.getAggregation("beginColumnPages")[0];
+                this.detailPage = this.fcl.getAggregation("midColumnPages")[0];
+                var that = this;
+
                 oOperation.setParameter("userInput", userInputForLLM).execute().then(function(event){
                     let oResults = oOperation.getBoundContext().getObject();
                     //let oTopicModel = new JSONModel({value:oResults.value, error_visible:false});
@@ -64,8 +89,18 @@ function (MessageBox, MessageToast, JSONModel){
                     //oTable.setBusyIndicatorDelay(0).setBusy(false);
                     let idTextAreaActions = oHBox.getAggregation("items")[0];
                     idTextAreaActions.setValue(oResults.value);
-                    idTextAreaActions.setVisible(true);
+                    idTextAreaActions.setVisible(false);
                     console.log(oResults);
+                    //update this
+                    llmResponse = oResults.value;
+                    let aActionsList=[], result= JSON.parse(oResults.value);
+                    if(result.PRE) aActionsList.push(result.PRE);
+                    if(result.MAIN) aActionsList.push(result.MAIN);
+                    if(result.POST) aActionsList.push(result.POST);
+                    let oLLMModel = new JSONModel({Actions: aActionsList});
+                    that.beginPage.setModel(oLLMModel, "LLM");
+                    that.beginPage.setVisible(true);
+                    console.log(that);
                 }).catch(function(error){
                     let oErrors = new JSONModel({value:[], error_visible: true, error_message:error.message});
                     console.log(oErrors);
@@ -73,14 +108,17 @@ function (MessageBox, MessageToast, JSONModel){
                 //let suggestedActions = 
             },
             oncreateActions: function(oEvent) {
+                /*
                 let oDialog1 = oEvent.getSource().getParent();
                 let oVBox = oDialog1.getAggregation("content")[1];
                 let oTextAreaActions = oVBox.getAggregation("items")[0];
                 const actionsInput = oTextAreaActions.getValue();
                 console.log(actionsInput);
-
+                */
                 //call create action and get output
-                let oDummyVBox = oDialog1.getAggregation("content")[2];
+                let oDialog1 = oEvent.getSource().getParent();
+                let oDummyVBox = oDialog1.getAggregation("content")[4];
+                const actionsInput = llmResponse;
                 let oOperation = oDummyVBox.getObjectBinding();
                 oOperation.setParameter("actionsInput", actionsInput).execute().then(function(event){
                     let oResults = oOperation.getBoundContext().getObject();
